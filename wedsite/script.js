ec2103d7c5867880236c9608b9a57e3871ab9f09
YAKUB-IT-DEV <yakuborsu@gmail.com>
2026-07-18 14:19:04 +0530

    Add files via upload

---
+++ wedsite/script.js
@@ -0,0 +1,16 @@
+// Smooth Scroll
+
+document.querySelectorAll("nav a").forEach(link=>{
+
+link.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
